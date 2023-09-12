5605d83a19dec92fbb869946a5b5509ee134fa54
amarell <mujagicamar@gmail.com>
2023-09-12 22:10:26 +0200

    API access setup

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+headers = {
+    "Authorization": "Bearer " + os.getenv("API_TOKEN"),
+}
 
 
 def run():
     r = requests.get(
-        "https://random-data-api.com/api/users/random_user",
-        headers={"Accept": "application/json"},
+        "https://lichess.org/api/account",
+        headers=headers,
     )
 
-    print(r.json())
+    print(json.dumps(r.json(), indent=4))
 
 
 if __name__ == "__main__":
