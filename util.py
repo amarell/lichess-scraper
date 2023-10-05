8d98e8a038c94bc4182b1f84ac34568c7fa5c705
amarell <mujagicamar@gmail.com>
2023-10-05 23:37:32 +0200

    Remove dead code

--- util.py
+++ util.py
@@ -38,11 +38,6 @@ def pretty_print_json(json_content):
     print(json.dumps(json_content, indent=4, sort_keys=True))
 
 
-# TODO
-def get_current_session_streak():
-    return
-
-
 def is_titled_player(user):
     return user.split(" ")[0] in [
         "GM",
@@ -58,8 +53,3 @@ def is_titled_player(user):
 
 def is_user_white(user, game):
     return game.white.username == user
-
-
-# TODO
-def is_user_banned(user):
-    return False
